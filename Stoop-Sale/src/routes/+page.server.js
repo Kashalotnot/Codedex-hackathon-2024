f5835f3a2f5a39a5b5785e66cddec2ee2c0c1b73
Berserk Chmonya <mauslut90@gmail.com>
2024-07-11 21:11:16 +0200

    Stoop Sale/Items getting from firebase

--- Stoop-Sale/src/routes/+page.server.js
+++ Stoop-Sale/src/routes/+page.server.js
@@ -1,4 +1,52 @@
+import { getDocs, collection } from 'firebase/firestore'
+import { db, storage } from '$lib/firebase/firebase'
+import { ref, getDownloadURL } from 'firebase/storage'
+
+async function updateItemsWithUrls(items) {
+    const promises = items.map(async (item) => {
+        let storageRef = ref(storage, item.image); 
+        let url = await getDownloadURL(storageRef);
+        item['image'] = url;
+    });
+
+    await Promise.all(promises);
+}
 
 export async function load() {
+    let stoopSale;
+    let items = [];
+
+    try {
+        let documentId;
+
+        const stoopSaleCollection = collection(db, 'Stoop-Sale'); 
+        const querySnapshot = await getDocs(stoopSaleCollection);
+
+        querySnapshot.forEach((doc) => {
+            if (!stoopSale || (stoopSale && doc.data().Date > new Date()  &&stoopSale.Date < doc.data().Date)) {
+                documentId = doc.id;
+                stoopSale = doc.data();
+            }
+        });
+        stoopSale.Date = stoopSale.Date.toDate();
+
+        const itemsCollection = collection(db, 'Stoop-Sale', documentId, 'Items');
+        const itemsQuerySnapshot = await getDocs(itemsCollection);
+
+        itemsQuerySnapshot.forEach((doc) => {
+            items.push(doc.data()); 
+        });
+
+        await updateItemsWithUrls(items);
+
+        return {
+            stoopSale: stoopSale,
+            items: items
+        };
+      
+
+    } catch (error) {
+        console.error('Error fetching stoop sale:', error);
+    }
    
 }
--- Stoop-Sale/src/routes/+page.svelte
+++ Stoop-Sale/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
     import { app } from '$lib/firebase/firebase.js';
+    export let data;
+
+    console.log(data);
 </script>
 
 <h1>Welcome to SvelteKit</h1>
