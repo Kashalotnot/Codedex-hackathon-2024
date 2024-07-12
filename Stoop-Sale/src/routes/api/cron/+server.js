85507dba38e9aeeb44d2c7922e21cc27981fc356
Berserk Chmonya <mauslut90@gmail.com>
2024-07-12 15:45:34 +0200

    verc config

--- Stoop-Sale/src/routes/api/cron/+server.js
+++ Stoop-Sale/src/routes/api/cron/+server.js
@@ -1,10 +1,5 @@
 import { store } from "$lib/store/store";
 
 export default function handler(req, res) {
-    if (req.method === 'GET') {
-        console.log('Scheduled GET request');
-        res.status(200).send('Hello, world!');
-    } else {
-        res.status(405).send('Method Not Allowed');
-    }
+    res.status(200).end('Hello Cron!');
 }
